a6ed50b783ae712aaba1aca2bb7c3e1fdb1002ef
hviswana <hviswanathan@linkedin.com>
2017-09-25 19:27:16 +0530

    Integrated mock and created component skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+// Header
+import { HeaderComponent } from './components/header/header.component';
+
+// Login
+import { LoginComponent } from './components/login/login.component';
+import { GameComponent } from './components/game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    GameComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
     HttpModule
