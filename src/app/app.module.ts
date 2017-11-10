8c2139f1d2352e047a9c2d6173933f5a3b210df3
hviswana <hviswanathan@linkedin.com>
2017-11-10 09:35:54 +0530

    Login page change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -22,7 +22,7 @@ import { GameComponent } from './components/game/game.component';
   imports: [
     AppRoutingModule,
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [],
