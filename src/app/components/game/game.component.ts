b12a48be2968fcc2fb7180419ee3e0a443c2f590
hviswana <hviswanathan@linkedin.com>
2017-11-10 09:35:54 +0530

    Login page change

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-game',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    // private router: Router,
+    ) {
   }
 
+
+  ngOnInit() {
+  //  console.log(this.router);
+  }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,23 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-login-form',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
+  form: FormGroup;
+
   constructor(
     private router: Router,
+    private formBuilder: FormBuilder,
     ) {
   }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      nickName: [null, Validators.required],
+      gameName: [null, Validators.required],
+    });
   }
 
   onLoginSubmit() {
-    this.router.navigate(['/game']);
+    const { nickName, gameName } = this.form.value;
+    this.router.navigate(['/game'], { queryParams :  this.form.value});
   }
 
 }
