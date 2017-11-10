6fd028b8743f6f57a12e0ac9450559bbbc521579
Arunkumar Srisailapathi <sail.arunkumar@gmail.com>
2017-11-10 10:54:38 +0530

    changes related to local storage and model

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+import { Player } from '../models/player.model';
+import { storeDetails } from '../utils/storage.utils';
 @Component({
   selector: 'app-login-form',
   templateUrl: './login.component.html',
@@ -10,23 +12,33 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   form: FormGroup;
+  player: string;
 
   constructor(
     private router: Router,
     private formBuilder: FormBuilder,
     ) {
+      this.player = '0';
   }
 
   ngOnInit() {
     this.form = this.formBuilder.group({
       nickName: [null, Validators.required],
       gameName: [null, Validators.required],
+      playerType: [null, Validators.required]
     });
   }
 
+  registerPlayer(player: Player): void {
+    if (storeDetails(player.nickName, JSON.stringify(player))) {
+      this.router.navigate(['/game'], { queryParams :  this.form.value});
+    }
+  }
+
   onLoginSubmit() {
-    const { nickName, gameName } = this.form.value;
-    this.router.navigate(['/game'], { queryParams :  this.form.value});
+    const { nickName, gameName, playerType } = this.form.value;
+    let player: Player = this.form.value;
+    this.registerPlayer(player);
   }
 
 }
---
+++ src/app/components/models/player.model.ts
@@ -0,0 +1,5 @@
+export class Player {
+    nickName: string;
+    gameName: string;
+    playerType: string;
+}
